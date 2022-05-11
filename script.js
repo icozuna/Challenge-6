603b31c5e4cc2c4ae48535647e7a1902553e1dc6
Izzy <isaiah.ozuna@yahoo.com>
2022-05-11 17:04:09 -0500

    able to pull more than one city in the console

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 let weather = {
   apiKey: "875d8b2bc5669857900a5eb3c0830a8c",
-  fetchWeather: function () {
+  fetchWeather: function (city) {
     fetch(
-      "https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=875d8b2bc5669857900a5eb3c0830a8c"
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&units=imperial&appid=" +
+        this.apiKey
     )
       .then((Response) => Response.json())
-      .then((data) => console.log(data));
+      .then((data) => this.displayWeather(data));
+  },
+  displayWeather: function (data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    console.log(name, icon, description, temp, humidity, speed);
+    document.querySelector("city").innerText = "Weather in" + name;
   },
 };
