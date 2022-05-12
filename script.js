895f291424db6102ca691c63b0bc256cde896a19
Izzy <isaiah.ozuna@yahoo.com>
2022-05-12 11:48:26 -0500

    pulled data from API onto console. Can only display from console.

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+var date = "2022-05-12";
+var format = "LLLL";
+var result = moment(date).format(format);
+document.querySelector("#date").innerText = date;
+
 let weather = {
   apiKey: "875d8b2bc5669857900a5eb3c0830a8c",
   fetchWeather: function (city) {
@@ -15,7 +20,16 @@ let weather = {
     const { icon, description } = data.weather[0];
     const { temp, humidity } = data.main;
     const { speed } = data.wind;
-    console.log(name, icon, description, temp, humidity, speed);
-    document.querySelector("city").innerText = "Weather in" + name;
+    const { dt } = data;
+    console.log(name, icon, description, temp, humidity, speed, dt);
+    document.querySelector(".city").innerText = "Weather in " + name + date;
+    document.querySelector(".icon").src =
+      "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".temp").innerText = temp + " °F";
+    document.querySelector(".humidity").innerText =
+      "Humidity: " + humidity + "%";
+    document.querySelector(".wind").innerText = " wind speed " + speed + " mph";
+    document.querySelector(".dt").innerText = dt;
   },
 };
