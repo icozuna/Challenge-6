6ce9f30e96476d0f3fddbdc972e6e5f43cf030ad
Izzy <isaiah.ozuna@yahoo.com>
2022-05-15 22:38:33 -0500

    final commit

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-var date = "2022-05-12";
+var date = moment().format(" dddd, MMMM Do YYYY, h:mm:ss a");
 var format = "LLLL";
-var result = moment(date).format(format);
 document.querySelector("#date").innerText = date;
 
 let weather = {
@@ -22,7 +21,7 @@ let weather = {
     const { speed } = data.wind;
     const { dt } = data;
     console.log(name, icon, description, temp, humidity, speed, dt);
-    document.querySelector(".city").innerText = "Weather in " + name + date;
+    document.querySelector(".city").innerText = "Weather in " + name;
     document.querySelector(".icon").src =
       "http://openweathermap.org/img/wn/" + icon + "@2x.png";
     document.querySelector(".description").innerText = description;
@@ -30,6 +29,33 @@ let weather = {
     document.querySelector(".humidity").innerText =
       "Humidity: " + humidity + "%";
     document.querySelector(".wind").innerText = " wind speed " + speed + " mph";
-    document.querySelector(".dt").innerText = dt;
+    document.querySelector(".input").addEventListener("click", function () {
+      data.search();
+    });
+  },
+  search: function () {
+    this.fetchWeather(document.querySelector(".input").value);
   },
 };
+document.querySelector(".button").addEventListener("click", function () {
+  weather.search();
+});
+
+weather.fetchWeather("Little Rock");
+/*var submit = document.getElementById("click-me");
+Element.addEventListener("click", function onClick() {
+  document.getElementById("#click-me").innerHTML = "Submit";
+});
+
+// from mask script
+/*let covid = {
+  apiKey: "597eafececmsh3c6a5faae1071d3p16f675jsnb3d0baf19e06",
+  fetchCovid: function (state) {
+    fetch(
+      "https://covid-19-statistics.p.rapidapi.com/reports?city_name=Autauga&region_province=Alabama&iso=USA&region_name=US&q=US%20Alabama&date=2020-04-16" +
+        this.apiKey
+    )
+      .then((Response) => Response.json())
+      .then((data) => this.displayState(data));
+  },
+};*/
